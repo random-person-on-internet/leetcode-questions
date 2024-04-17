293222a7cc13cf3e59993142aad19715f3338ba0
random-person-on-internet <vedlakkad05@gmail.com>
2024-04-17 22:22:45 +0530

    QUESTION 1470

---
+++ 1470.ShuffleTheArray.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> shuffle(vector<int>& nums, int n) {
+        vector<int> ans;
+
+        for (int i = 0; i < n; i++) {
+            ans.push_back(nums[i]);
+            ans.push_back(nums[n + i]);
+        }
+
+        return ans;
+}
+
+int main(){
+	// update if necesary
+	
+	return 0;
+}
